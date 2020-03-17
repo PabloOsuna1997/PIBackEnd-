0c9f21f69d34e5b66259ee6390e01782d1106309
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-03-17 11:57:58 +0000

    Initial commit

---
+++ PIbackEnd/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+
+//setting
+app.set('port',process.env.PORT || 3000);
+
+//midlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors({origin: 'url'}));
+app.use(require('./routes/index.js'));
+
+
+
+app.listen(app.get('port'), () =>{
+    console.log('server is listening');
+})

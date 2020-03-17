71c58f0238b338f3848f768aaeb6f9c71adb0482
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-03-17 12:22:21 +0000

    init

--- PIbackEnd/routes/index.js
+++ PIbackEnd/routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const route = express.Router();
 const controllerLogin = require('../controllers/login.controller.js');
 
-route.get('/', controllerLogin.get);
+route.get('/hello', controllerLogin.get);
 
 module.exports = route;

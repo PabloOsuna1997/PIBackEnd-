c6f9895420993762f262f81d860b6828437f4a28
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-03-17 12:06:37 +0000

    Initial commit

---
+++ PIbackEnd/controllers/login.controller.js
@@ -0,0 +1,8 @@
+const login = {};
+
+login.get = (req,res) =>{
+    console.log('say hello');
+    res.send('say hello');
+}
+
+module.exports = login;
--- PIbackEnd/routes/index.js
+++ PIbackEnd/routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const route = express.Router();
+const controllerLogin = require('../controllers/login.controller.js');
 
-route.get('/',(req,res) =>{
-    res.send('hello');
-});
+route.get('/', controllerLogin.get);
 
 module.exports = route;

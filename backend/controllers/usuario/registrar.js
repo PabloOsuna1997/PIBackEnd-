c1a75426f235b9dbab8412dbcdf0ae22bce1c054
PabloOsuna1997 <2472932372001@ingenieria.usac.edu.gt>
2020-04-19 00:46:25 -0600

    fix: Relacionar usuario a varios roles

--- backend/controllers/usuario/registrar.js
+++ backend/controllers/usuario/registrar.js
@@ -23,6 +23,30 @@ module.exports = function (router) {
         } catch (error) {
             res.message = error;
             if (error.code == 'ER_DUP_ENTRY') {
+                const data = await db.query('SELECT rol FROM ROL_USUARIO WHERE correo = ?', [req.body.correo]);
+                var existe = false;
+
+                for (const element of data) { //recorremos todas las sedes en las que se encuentra el cliente
+
+                    var rol = Object.assign({}, element);
+                    console.log(rol);
+                    if (rol.rol == req.body.rol) { //verifico que no sea la misma sede a la que quiere registrarse
+                        existe = true;
+                        break;
+                    }
+                }
+                if (existe) { //si no existe ese usuario
+                    res.status(400).send({ mensaje: 'El usuario ya se encuentra registrado con este rol.' });
+                } else {
+                    const entradaRolUsuario = require('../../src/mapeoObjetos/usuario/entradaRolUsuario');
+                    const ress = await db.query('INSERT INTO ROL_USUARIO set ?', [entradaRolUsuario(req.body).data]);
+
+                    if (ress.affectedRows > 0) {
+                        res.status(200).send({ mensaje: 'Usuario registrado.' });
+                    } else {
+                        res.status(400).send({ mensaje: 'Usuario registrado pero no se pudo relacionar con los roles.' });
+                    }
+                }
                 res.status(400).send({ mensaje: 'El correo ya se encuentran en uso.' });
             } else {
                 res.status(500).send({ mensaje: 'No se pudo completar la solicitud' });

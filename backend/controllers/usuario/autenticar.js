e2dfc01ab7ac739ab55c78b00f29b07e3f8eb02d
PabloOsuna1997 <2472932372001@ingenieria.usac.edu.gt>
2020-04-19 01:29:17 -0600

    docs: Identacion de codigo

--- backend/controllers/usuario/autenticar.js
+++ backend/controllers/usuario/autenticar.js
@@ -1,21 +1,21 @@
 const db = require('../../src/dataBase/conexion');
 module.exports = function (router) {
 
-    router.post('/', async(req, res) => {
-       try{
-           const entradaAutenticar = require('../../src/mapeoObjetos/usuario/entradaAutenticar');
+    router.post('/', async (req, res) => {
+        try {
+            const entradaAutenticar = require('../../src/mapeoObjetos/usuario/entradaAutenticar');
 
             const data = entradaAutenticar(req.body).data;
-            const result = await db.query('SELECT * FROM USUARIO WHERE correo = ? AND password = ?;',[data.correo,data.password]);
-           
-            if(result.length > 0){
+            const result = await db.query('SELECT * FROM USUARIO WHERE correo = ? AND password = ?;', [data.correo, data.password]);
+
+            if (result.length > 0) {
+                //fuera bueno tambien retornar el tipo de usuario
                 res.status(200).send({ mensaje: 'Bienvenido', result });
-            }else{
+            } else {
                 res.status(400).send({ mensaje: 'Credenciales no validas' });
             }
 
-        
-        }catch(error){
+        } catch (error) {
             res.message = error;
             res.status(500).send({ mensaje: 'No se pudo completar la solicitud' });
         }

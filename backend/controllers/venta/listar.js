60859745e5e5a7f5fc1f121e1e31158a8efb0838
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-04-24 23:03:46 +0000

    feat: validar ventas y sedes vacias

--- backend/controllers/venta/listar.js
+++ backend/controllers/venta/listar.js
@@ -3,12 +3,12 @@ const db = require('../../src/dataBase/conexion');
 module.exports = function (router) {
     router.get('/', async (req, res) => {
         try {
-            const result = await db.query('SELECT * FROM SEDE;');
+            const result = await db.query('SELECT * FROM VENTA;');
 
             if (result.length > 0) {
                 res.status(200).send({ clientes: result });
             } else {
-                res.status(200).send({ mensaje: 'Actualmente no existen sedes.' });
+                res.status(200).send({ mensaje: 'Actualmente no existen ventas.' });
             }
         } catch (error) {
             res.status(500).send({ mensaje: 'No se pudo completar la solicitud' });

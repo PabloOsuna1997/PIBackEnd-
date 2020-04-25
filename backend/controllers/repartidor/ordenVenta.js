ec5911bc311880ebe9dc788223eb9a5687451d2a
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-04-25 19:47:29 +0000

    fix: Listar Usuarios

---
+++ backend/controllers/repartidor/ordenVenta.js
@@ -0,0 +1,19 @@
+const db = require('../../src/dataBase/conexion');
+
+module.exports = function (router) {
+    router.get('/:correo', async (req, res) => {
+        try {
+            const result = await db.query('SELECT * FROM VENTA WHERE repartidor = ? AND estado_repartidor = 0', [req.params.correo]);
+            if (result.length > 0) {
+                res.status(200).send({ ventas: result });
+            } else {
+                res.status(200).send({ mensaje: 'El repartidor indicado no tiene entregas pendientes.' });
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({ mensaje: 'No se puede completar la solicitud.' });
+
+        }
+    });
+
+};

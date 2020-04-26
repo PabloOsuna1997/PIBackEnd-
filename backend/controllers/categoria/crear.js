32a5a44a2dc4c130730bc8bdb03de3cb3ae7ef0a
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-04-26 10:09:40 +0000

    fix: atributo nombre en el insert de categoria

--- backend/controllers/categoria/crear.js
+++ backend/controllers/categoria/crear.js
@@ -3,13 +3,14 @@ const db = require('../../src/dataBase/conexion');
 module.exports = function (router) {
     router.post('/', async (req, res) => {
         try {
-            const result = await db.query('INSERT INTO CATEGORIA set ? ', [req.body.nombre]);
+            const result = await db.query('INSERT INTO CATEGORIA set nombre = ? ', [req.body.nombre]);
             if(result.affectedRows > 0){
                 res.status(200).send({mensaje: 'Categoria creada.'});
             }else{
                 res.status(200).send({mensaje: 'No se pudo ingresar la categoria.'});
             }
         }catch(error){
+            console.log(error);
             res.status(500).send({mensaje: 'No se pudo completar la solicitud.'});
         }
     });

c68c21844f89b6ce440446211b172ab5244c80cc
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-04-28 19:54:48 +0000

    feat: Agregar bodegas a listar usuario

--- backend/controllers/usuario/listarUsuario.js
+++ backend/controllers/usuario/listarUsuario.js
@@ -4,14 +4,21 @@ module.exports = function (router) {
     router.get('/:correo', async (req, res) => {
         try {
             const result = await db.query('SELECT * FROM USUARIO WHERE correo = ?;',[req.params.correo]);
+           
             for (let index = 0; index < result.length; index++) {
                 var user = Object.assign({}, result[index]);
+                
                 const roles = await db.query(`SELECT r.rol, ro.nombre 
                 FROM ROL_USUARIO r INNER JOIN ROL ro 
                 ON r.rol = ro.rol WHERE correo = ?`, [user.correo]);
+                
+
+                const bodegas = await db.query('SELECT * FROM BODEGA WHERE encargado = ?',[req.params.correo]);
+
                 result[index] = {
                     ...user,
-                    roles
+                    roles,
+                    bodegas
                 }
             }
             if (result.length > 0) {

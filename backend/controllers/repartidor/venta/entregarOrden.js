d9a8f86e54b1cb44d6899d1ec88dc6b1013d1efa
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-04-28 18:53:48 +0000

    feat: Actualizacion inventario en ventas y entrega

--- backend/controllers/repartidor/venta/entregarOrden.js
+++ backend/controllers/repartidor/venta/entregarOrden.js
@@ -3,12 +3,36 @@ const db = require('../../../src/dataBase/conexion');
 module.exports = function (router) {
     router.put('/:correo/:venta', async (req, res) => {
         try {
-            const result = await db.query('UPDATE VENTA set estado_repartidor = 1 WHERE repartidor = ? AND codigo_venta = ?', [req.params.correo,req.params.venta]);
+            const result = await db.query('UPDATE VENTA set estado_repartidor = 1 WHERE repartidor = ? AND codigo_venta = ? AND estado_repartidor = 0', [req.params.correo, req.params.venta]);
             if (result.affectedRows > 0) {
                 //actualizar el inventario
+
+                const productos = await db.query('SELECT * FROM DETALLE_VENTA WHERE venta = ?', [req.params.venta]);
+
+                for (var index = 0; index < productos.length; index++) {
+                    var element = Object.assign({}, productos[index]);
+                    //venta local debo actualizar el invetario de inmediato
+                    var updateOrigen = await db.query('UPDATE DETALLE_INVENTARIO SET cantidad_antigua = cantidad_nueva, cantidad_nueva = cantidad_nueva - 1 WHERE inventario = ? AND producto = ?',
+                        [element.codigo_inventario, element.producto]);
+
+                    var det = await db.query('SELECT * FROM DETALLE_INVENTARIO  WHERE inventario = ? AND producto = ?',
+                        [element.codigo_inventario, element.producto]);
+                    di = Object.assign({}, det[0]);
+
+                    //insertamos el log
+                    req.body.correoUsuario = req.params.correo;
+                    req.body.motivo = "Venta a domicilio.";
+                    req.body.skuProducto = element.producto;
+                    req.body.cantidadNueva = di.cantidad_nueva;
+                    //log de actualizacion de inventario
+                    //origen
+                    const logInventario = require('../../../src/mapeoObjetos/bodega/inventario/logInventario');
+                    var log = await db.query('INSERT INTO LOG_ACTUALIZACION set ?', [logInventario(req.body, element.bodega, element.codigo_inventario, di.cantidad_antigua).data]);
+                }
+
                 res.status(200).send({ mensaje: 'Venta entregada' });
             } else {
-                res.status(400).send({ mensaje: 'El correo indicado no existe o la venta no existe' });
+                res.status(400).send({ mensaje: 'El correo indicado no existe o la venta no existe, o esta orden ya fue entregada.' });
             }
 
         } catch (error) {
--- backend/controllers/venta/crear.js
+++ backend/controllers/venta/crear.js
@@ -3,19 +3,20 @@ const db = require('../../src/dataBase/conexion');
 module.exports = function (router) {
     router.post('/', async (req, res) => {
         try {
-            
+
             const entradaVenta = require('../../src/mapeoObjetos/venta/entradaCrear');
             //validar usuario repartidor
             const vendedor = await db.query('SELECT rol FROM ROL_USUARIO WHERE correo = ?', [req.body.correoVendedor]);
             if (vendedor.length <= 0) {
                 res.status(400).send({ mensaje: 'El vendedor no existe.' });
             }
+
             if (req.body.correoRepartidor != '') {
                 const repartidor = await db.query('SELECT rol FROM ROL_USUARIO WHERE correo = ?', [req.body.correoRepartidor]);
                 if (repartidor.length <= 0) {
                     res.status(400).send({ mensaje: 'El repartidor no existe.' });
                 }
-            }else{
+            } else {
                 req.body.correoRepartidor = null;
                 req.body.fechaEntrega = null;
                 req.body.tipoVenta = 0;
@@ -24,22 +25,45 @@ module.exports = function (router) {
 
             const result = await db.query('INSERT INTO VENTA set ?', [entradaVenta(req.body).data]);
             const venta = Object.assign({}, result);
-            
+
             if (result.affectedRows > 0) {
                 //insertar detalle venta
                 var status = 0;
                 for (const element of req.body.productos) {
                     const entradaVentaProducto = require('../../src/mapeoObjetos/venta/entradaVentaProducto');
-                    const ress = await db.query('INSERT INTO DETALLE_VENTA set ?', [entradaVentaProducto(element,venta.insertId).data]);
+                    const ress = await db.query('INSERT INTO DETALLE_VENTA set ?', [entradaVentaProducto(element, venta.insertId).data]);
 
                     if (ress.affectedRows > 0) {
+                        //actualizar el inventario
+                        if (req.body.tipoVenta == 0) {
+                            //venta local debo actualizar el invetario de inmediato
+                            const updateOrigen = await db.query('UPDATE DETALLE_INVENTARIO SET cantidad_antigua = cantidad_nueva, cantidad_nueva = cantidad_nueva - 1 WHERE inventario = ? AND producto = ?',
+                                [element.idInventario, element.skuProducto]);
+
+                            const det = await db.query('SELECT * FROM DETALLE_INVENTARIO  WHERE inventario = ? AND producto = ?',
+                                [element.idInventario, element.skuProducto]);
+                            di = Object.assign({}, det[0]);
+
+                            //insertamos el log
+                            req.body.correoUsuario = req.body.correoVendedor;
+                            req.body.motivo = "Venta local.";
+                            req.body.skuProducto = element.skuProducto;
+                            req.body.cantidadNueva = di.cantidad_nueva;
+                            //log de actualizacion de inventario
+                            //origen
+                            const logInventario = require('../../src/mapeoObjetos/bodega/inventario/logInventario');
+                            var log = await db.query('INSERT INTO LOG_ACTUALIZACION set ?', [logInventario(req.body, element.idBodega, di.inventario, di.cantidad_antigua).data]);
+
+
+                        }
                         status = 200;
                     } else {
                         status = 400;
                     }
                 }
                 if (status == 200) {
-                    //actualizar el inventario
+
+
                     res.status(200).send({ mensaje: 'Venta Agregada con exito.' });
                 } else {
                     res.status(400).send({ mensaje: 'Venta Agregada con exito pero alguno de sus productos no pudo registrarse.' });
@@ -49,9 +73,9 @@ module.exports = function (router) {
             }
         } catch (error) {
             console.log(error)
-            if(error.code == 'ER_NO_REFERENCED_ROW_2'){
+            if (error.code == 'ER_NO_REFERENCED_ROW_2') {
                 res.status(400).send({ mensaje: 'El nit del cliente no se encuentra registrado en ninguna de nuestras sedes.' });
-            }else{
+            } else {
                 res.status(500).send({ mensaje: 'No se pudo completar la solicitud.' });
             }
         }

01ce8edb9c937f4e403af702395cdf7d2a5a9d2f
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-04-29 11:29:19 +0000

    feat: Agregar nombre producto a visualizar transferencia

--- backend/controllers/transferencia/visualizar.js
+++ backend/controllers/transferencia/visualizar.js
@@ -5,6 +5,13 @@ module.exports = function (router) {
         try {
             //en estado de solicitud
             const result = await db.query('SELECT * FROM TRANSFERENCIA WHERE estado_transferencia = 0;');
+            
+            for (let index = 0; index < result.length; index++) { 
+                var trans = Object.assign({}, result[index]);
+                const nombre = await db.query('SELECT nombre FROM PRODUCTO WHERE sku = ?',[trans.producto]);
+                const nameP = Object.assign({}, nombre[0]);
+                result[index].nombre = nameP.nombre;              
+            } 
             if (result.length > 0) {
                 res.status(200).send({ transferencias: result });
             } else {

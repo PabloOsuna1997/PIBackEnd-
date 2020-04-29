0148904c733685e356c138bfcfa74e3af08b99f1
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-04-29 09:56:13 +0000

    fix: validar cantidad cero en stock

--- backend/controllers/repartidor/venta/entregarOrden.js
+++ backend/controllers/repartidor/venta/entregarOrden.js
@@ -3,6 +3,21 @@ const db = require('../../../src/dataBase/conexion');
 module.exports = function (router) {
     router.put('/:correo/:venta', async (req, res) => {
         try {
+            //verificacion que haya los productos necesarios 
+            /* const verificacion = await db.query('SELECT * FROM DETALLE_VENTA WHERE venta = ?', [req.params.venta]);
+             for (var index = 0; index < verificacion.length; index++) {
+                 var element = Object.assign({}, verificacion[index]);
+ 
+                 var det = await db.query('SELECT * FROM DETALLE_INVENTARIO  WHERE inventario = ? AND producto = ?',
+                     [element.codigo_inventario, element.producto]);
+                 di = Object.assign({}, det[0]);
+ 
+                 if(di.cantidad_nueva >= 1){
+                     //si es mayor o igual a 1 es que puedo disminuir un elemento.
+ 
+                 }
+             }*/
+
             const result = await db.query('UPDATE VENTA set estado_repartidor = 1 WHERE repartidor = ? AND codigo_venta = ? AND estado_repartidor = 0', [req.params.correo, req.params.venta]);
             if (result.affectedRows > 0) {
                 //actualizar el inventario
@@ -11,23 +26,33 @@ module.exports = function (router) {
 
                 for (var index = 0; index < productos.length; index++) {
                     var element = Object.assign({}, productos[index]);
-                    //venta local debo actualizar el invetario de inmediato
-                    var updateOrigen = await db.query('UPDATE DETALLE_INVENTARIO SET cantidad_antigua = cantidad_nueva, cantidad_nueva = cantidad_nueva - 1 WHERE inventario = ? AND producto = ?',
+                    //validacion para numeros negativos
+                    var deta_ = await db.query('SELECT * FROM DETALLE_INVENTARIO  WHERE inventario = ? AND producto = ?',
                         [element.codigo_inventario, element.producto]);
+                    deta = Object.assign({}, deta_[0]);
 
-                    var det = await db.query('SELECT * FROM DETALLE_INVENTARIO  WHERE inventario = ? AND producto = ?',
-                        [element.codigo_inventario, element.producto]);
-                    di = Object.assign({}, det[0]);
+                    if (deta.cantidad_nueva >= 1) {
+                        //si es mayor o igual a 1 es que puedo disminuir un elemento.
 
-                    //insertamos el log
-                    req.body.correoUsuario = req.params.correo;
-                    req.body.motivo = "Venta a domicilio.";
-                    req.body.skuProducto = element.producto;
-                    req.body.cantidadNueva = di.cantidad_nueva;
-                    //log de actualizacion de inventario
-                    //origen
-                    const logInventario = require('../../../src/mapeoObjetos/bodega/inventario/logInventario');
-                    var log = await db.query('INSERT INTO LOG_ACTUALIZACION set ?', [logInventario(req.body, element.bodega, element.codigo_inventario, di.cantidad_antigua).data]);
+                        //venta local debo actualizar el invetario de inmediato
+                        var updateOrigen = await db.query('UPDATE DETALLE_INVENTARIO SET cantidad_antigua = cantidad_nueva, cantidad_nueva = cantidad_nueva - 1 WHERE inventario = ? AND producto = ?',
+                            [element.codigo_inventario, element.producto]);
+
+                        var det = await db.query('SELECT * FROM DETALLE_INVENTARIO  WHERE inventario = ? AND producto = ?',
+                            [element.codigo_inventario, element.producto]);
+                        di = Object.assign({}, det[0]);
+
+                        //insertamos el log
+                        req.body.correoUsuario = req.params.correo;
+                        req.body.motivo = "Venta a domicilio.";
+                        req.body.skuProducto = element.producto;
+                        req.body.cantidadNueva = di.cantidad_nueva;
+                        //log de actualizacion de inventario
+                        //origen
+                        const logInventario = require('../../../src/mapeoObjetos/bodega/inventario/logInventario');
+                        var log = await db.query('INSERT INTO LOG_ACTUALIZACION set ?', [logInventario(req.body, element.bodega, element.codigo_inventario, di.cantidad_antigua).data]);
+
+                    }
                 }
 
                 res.status(200).send({ mensaje: 'Venta entregada' });
